6f525f1747c474e4d41c972d5449bf5387da829a
Linus Graybill <graybill@gmail.com>
2011-03-06 00:49:21 -0500

    Breaking out into separate functions

--- roksbox-xml.rb
+++ roksbox-xml.rb
@@ -10,27 +10,27 @@ VIDEO_PATH = "/Volumes/MAXTOR (F)/Video"
 SERVER_PATH = "F:\\\\"
 
 # Global values
-sub_directories = []
-s_sub_directories = []
-video_files = []
+@sub_directories = []
+@s_sub_directories = []
+@video_files = []
 
 Dir.chdir(VIDEO_PATH) #TODO move to setup()
 
 def movie_node_template(movie)
-    "<movie>
-      <origtitle>#{movie_title(movie)}</origtitle>
-      <year></year>
-      <genre>#{genres(movie)}</genre>
-      <mpaa></mpaa>
-      <director></director>
-      <actors></actors>
-      <description></description>
-      <path>#{web_server_path(movie)}</path>
-      <length></length>
-      <videocodec>#{video_codec(movie)}</videocodec>
-      <poster>#{poster_path(movie)}</poster>
-    </movie>
-    "
+  "<movie>
+    <origtitle>#{movie_title(movie)}</origtitle>
+    <year></year>
+    <genre>#{genres(movie)}</genre>
+    <mpaa></mpaa>
+    <director></director>
+    <actors></actors>
+    <description></description>
+    <path>#{web_server_path(movie)}</path>
+    <length></length>
+    <videocodec>#{video_codec(movie)}</videocodec>
+    <poster>#{poster_path(movie)}</poster>
+  </movie>
+  "
 end
 
 def web_server_path(file_path)
@@ -38,6 +38,10 @@ def web_server_path(file_path)
   file_path.gsub('/', '\\')
     .gsub('\Volumes\MAXTOR (F)', 'F:\\\\')
     .gsub('&', '&amp;')
+end
+
+def file_path(file)
+  Dir.pwd() + "/" + file
 end
 
 def movie_title(file_path)
@@ -48,7 +52,7 @@ def movie_title(file_path)
 end
 
 def genres(file_path)
-  # TODO Get sub genres
+  # TODO Get sub genres, anything after Video and before extname
   file_path.split('/')[-2]
 end
 
@@ -59,9 +63,9 @@ def valid_video_format(file_path)
 end
 
 def valid_video_title(file_path)
-  # FIXME
+  # FIXME Not hiding . files
   r = Regexp.new('^._') # skip hidden files
-  return true unless file_path.match(r)
+  file_path.match(r)
 end
 
 def poster_path(file_path)
@@ -75,80 +79,64 @@ def video_codec(file_path)
   File.extname(file_path).gsub('.', '')
 end
 
-Dir['*/'].each do | dir|
-  unless dir == "images/"
-    sub_directories << "#{VIDEO_PATH}/#{dir}"
-  end
-end
-
 def setup
-  
-end
-
-def parse_directory(dir)
-  
+  Dir['*/'].each do | dir|
+    unless dir == "images/"
+      @sub_directories << "#{VIDEO_PATH}/#{dir}"
+    end
+  end
 end
 
 def generate_xml_file
-  
-end
-
-def generate_xml_node(movie)
-  
-end
-
-
-# TODO break into a function
-sub_directories.each do |s|
-  Dir.chdir(s)
-  Dir.entries(Dir.pwd).each do |i|
-    s_sub_directories = []
-    f_path = Dir.pwd() + "/" + i
-    f = File.absolute_path(f_path)
-    if !File.directory?(f)
-      if valid_video_format(f)
-        video_files << f
-      else
-        # p "Check on video" + f_path
-      end
-    else
-      s_sub_directories << "#{f_path}"
-    end
-    # loop through sub_directories?
-    # they will have genre subdirectories
-    s_sub_directories.each do |d|
-      Dir.chdir(d)
-      Dir.entries(Dir.pwd).each do |h|
-        f_path = Dir.pwd() + "/" + h
-        p f_path
-        f = File.absolute_path(f_path)
-        if !File.directory?(f)
-          if valid_video_format(f)
-            # FIXME This is just duplicating shit right now
-            video_files << f
-          end
-        end
-
-      end
-    end
-
-  end
-
-
   # TODO Move to generate_xml_file
   File.open("#{VIDEO_PATH}/video.xml", 'w') { |f| 
     f.write('<xml>
     <viddb>
     ')
-    video_files.each do |v|
-      # TODO Move to generate_xml_node
-      f.write(movie_node_template(v)) 
+    @video_files.each do |v|
+      f.write(generate_xml_node(v)) 
     end
     f.write('</viddb>
   </xml>')
   }
-
 end
+
+def generate_xml_node(movie)
+  movie_node_template(movie)
+end
+
+def parse_directories
+  @sub_directories.each do |s|
+    Dir.chdir(s)
+    parse_directory(Dir.pwd())
+  end
+end
+
+def parse_directory(dir)
+  Dir.entries(Dir.pwd).each do |i|
+    @s_sub_directories = []
+    f_path = file_path(i)
+    f = File.absolute_path(f_path)
+    if !File.directory?(f)
+      if valid_video_format(f)
+        @video_files << f
+      else
+        p "Check on video" + f_path
+      end
+    else
+      @s_sub_directories << "#{f_path}"
+    end
+    # loop through sub_directories?
+    # they will have genre subdirectories
+
+  end #end Dir.entries
+  
+end
+
+setup
+parse_directories
+generate_xml_file
+
 
 
 
