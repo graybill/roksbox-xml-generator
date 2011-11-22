10f617ce6086719ecc57f22708e27f52912d0330
Linus Graybill <graybill@gmail.com>
2011-11-21 21:24:30 -0500

    Episode class and test

---
+++ lib/episode.rb
@@ -0,0 +1,5 @@
+class Episode
+  include HTTParty
+  attr_accessor :name, :status, :rating, :url
+
+end
---
+++ test/episode_test.rb
@@ -0,0 +1,7 @@
+require File.expand_path("#{File.dirname(__FILE__)}/test_helper")
+require "#{BASE_PATH}/lib/config"
+require "#{BASE_PATH}/lib/episode"
+
+class EpisodeTest < Test::Unit::TestCase
+
+end
