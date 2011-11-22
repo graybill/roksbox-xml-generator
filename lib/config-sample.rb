d0101d55bc5309ffacaa5b9e9ec56d8bcbe169c4
Linus Graybill <graybill@gmail.com>
2011-11-21 21:37:49 -0500

    Config file

---
+++ lib/config-sample.rb
@@ -0,0 +1,5 @@
+# TMDB_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TVDB_API_KEY = "xxxxxxxxxxxxxxxx"
+
+VIDEOS_PATH = "/Volumes/MAXTOR (F)/Video/" # root path for videos
+ROKSBOX_PATH = "F:\\\\" # root path roksbox is looking for
