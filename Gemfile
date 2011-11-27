275ffd8d59f396b2e7c9a70ecd816cfb00d0bc30
Linus Graybill <graybill@gmail.com>
2011-11-26 23:56:22 -0500

    Require fakeweb

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ gem 'httparty', '0.8.1'
 group :test do
   gem 'aruba', '0.4.6'
   gem 'cucumber', '~> 1.1.2'
+  gem 'fakeweb'
   gem 'fakefs'
   gem 'nokogiri'
-  gem 'redgreen'
+  gem 'redgreen', '1.2.2'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,5 @@ BASE_PATH = File.expand_path("#{File.dirname(__FILE__)}/..")
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'fakefs/safe'
 require 'test/unit'
+require 'fakeweb'
 require 'httparty'
